7422838ff54acd6c3705ccb16211b38271440d2d
Gregor Martynus <gregor@martynus.net>
2013-12-27 02:24:59 +0100

    proper grunt setup

---
+++ Gruntfile.js
@@ -0,0 +1,201 @@
+var livereloadSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet;
+var mountFolder = function (connect, dir) {
+  return connect.static(require('path').resolve(dir));
+};
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+    // load app meta info
+    pkg: grunt.file.readJSON('package.json'),
+
+    // js hint Gruntfile & src files
+    jshint: {
+
+      gruntfile: {
+        src: ['gruntfile.js']
+      },
+
+      server: {
+        src: ['src/app/**/*.js']
+      }
+    },
+
+    // run jshint whenever a *.js file changes
+    watch: {
+      jshint: {
+        files: ['<%= jshint.files %>'],
+        tasks: ['jshint']
+      },
+      server: {
+        options: {
+          livereload: true
+        },
+        files: [
+          'src/index.html',
+          'src/app/**/*.js',
+          'src/assets/**',
+          'src/data/*.json'
+        ],
+        tasks: ['jshint:server']
+      }
+    },
+
+    connect: {
+      server: {
+        options: {
+          port: 9000,
+          base: 'src/',
+          open: true,
+          livereload: true,
+          middleware: function(connect) {
+            return [
+              // https://github.com/intesso/connect-livereload#grunt-example
+              livereloadSnippet,
+
+              // load assets from .tmp/ folder first, fallback to src/
+              mountFolder(connect, '.tmp'),
+              mountFolder(connect, 'src')
+            ];
+          }
+        }
+      },
+      build: {
+        options: {
+          port: 3000,
+          base: 'build/',
+          open: true,
+          keepalive: true
+        }
+      }
+    },
+    // remove build folder before new build
+    clean: {
+      build: {
+        src: ['build', '.tmp']
+      }
+    },
+
+    // copy static assets from src & build folder
+    copy: {
+      build: {
+        files: [{
+          expand: true,
+          cwd: 'src/',
+          src: [
+            'index.html',
+            'data/*.json',
+            'assets/**'
+          ],
+          dest: 'build/'
+        }]
+      }
+    },
+
+    // concant & minify assets based on HTML blocks
+    // https://github.com/yeoman/grunt-usemin
+    useminPrepare: {
+      html: 'src/index.html',
+      options: {
+        dest: 'build'
+      }
+    },
+
+    // replace blocks of script / style tags with
+    // their concatinated & minified versions
+    usemin: {
+      html: 'build/index.html'
+    },
+
+    // Static asset revisioning through file content hash
+    // https://github.com/yeoman/grunt-filerev
+    filerev: {
+      options: {
+        encoding: 'utf8',
+        algorithm: 'md5',
+        length: 8
+      },
+      assets: {
+        src: [
+          // do not far-future manifest.appcache:
+          // http://alistapart.com/article/application-cache-is-a-douchebag#section7
+          'build/**/*.js',
+          'build/**/*.css',
+          'build/**/*.png'
+        ]
+      }
+    },
+
+    appcache: {
+      options: {
+        basePath: 'build'
+      },
+      all: {
+        dest: 'build/manifest.appcache',
+        cache: 'build/**',
+        network: '*',
+        fallback: '/ /'
+      }
+    },
+
+    // dynamically add manifest="manifest.appcache" to <html> tag
+    replace: {
+      build: {
+        src: ['build/index.html'],
+        overwrite: true,
+        replacements: [{
+          from: /<html([^>]*)>/,
+          to: '<html$1 manifest="manifest.appcache">'
+        }]
+      }
+    },
+
+    // bump version of app
+    bump: {
+      options: {
+        files: ['package.json', 'bower.json'],
+        updateConfigs: [],
+        commit: true,
+        commitMessage: 'Release v%VERSION%',
+        commitFiles: ['package.json', 'bower.json'], // '-a' for all files
+        createTag: true,
+        tagName: 'v%VERSION%',
+        tagMessage: 'Version %VERSION%',
+        push: true,
+        pushTo: 'origin'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  // https://github.com/yeoman/grunt-usemin
+  grunt.loadNpmTasks('grunt-usemin');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // https://github.com/yeoman/grunt-filerev
+  grunt.loadNpmTasks('grunt-filerev');
+
+  // https://github.com/canvace/grunt-appcache
+  grunt.loadNpmTasks('grunt-appcache');
+
+  // https://github.com/yoniholmes/grunt-text-replace
+  grunt.loadNpmTasks('grunt-text-replace');
+
+  grunt.loadNpmTasks('grunt-bump');
+
+
+  // Default task(s).
+  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('build', ['jshint:server', 'clean', 'useminPrepare', 'copy', 'concat', 'uglify', 'cssmin', 'filerev', 'appcache', 'replace:build', 'usemin']);
+  grunt.registerTask('server', ['connect:server', 'watch:server']);
+};
---
+++ README.md
@@ -0,0 +1,20 @@
+Jana's Map Quiz
+=================
+
+A simple map-based quiz to get to know our world a little
+better, every day. It's open source, build with web technologies
+and works on any device.
+
+## Keyboard-Shortcuts
+
+The game is optimized for both, touch & keyboard usage. The
+following keybord shortcuts are supported:
+
+`+` / `-`: zoom in / out
+`1` - `4`: select answer 1 - 4
+`a` - `z`: select answer by first letter
+
+## Happy Quizzing!
+
+**© 2013 Gregor Martynus  **
+MIT Licensed
