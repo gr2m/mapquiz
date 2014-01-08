d96d8a3f29a487d1616c63cdcd9deb90e5ba002a
Gregor Martynus <gregor@martynus.net>
2014-01-08 18:30:51 +0100

    Updates

---
+++ vendor.d7bfe36d.js
@@ -0,0 +1,16 @@
+// this file is only used as a workaround for
+// requirjs' build optimization. It defines the
+// files that will be combined into vendor.js
+define([
+  'text',
+  'hbs',
+  'jquery',
+  'handlebars',
+  'marionette',
+  'lodash',
+  'backbone',
+  'leaflet',
+  'fittext',
+  'mousetrap',
+  'fastclick'
+], function(){});
